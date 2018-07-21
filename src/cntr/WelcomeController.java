b371ef1750551c9fc2cd9a6388b61cfed98d926f
mevishalss <mevishalss@gmail.com>
2018-07-21 14:46:44 +0530

    hibernate connection done

--- src/cntr/WelcomeController.java
+++ src/cntr/WelcomeController.java
@@ -1,14 +1,28 @@
 package cntr;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import Dao.UserDao;
 import dto.User;
 
 @Controller
 public class WelcomeController {
+	@Autowired
+	UserDao dao;
+	
+	public UserDao getDao() {
+		return dao;
+	}
+
+	public void setDao(UserDao dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping(value="/prepLog.php")
 	public String prepLogin(ModelMap model) {
 		model.put("user", new User());
@@ -17,6 +31,7 @@ public class WelcomeController {
 	
 	@RequestMapping(value="/login.php")
 	public String login(User user,ModelMap model) {
+		dao.createUser(user);
 		model.put("msg", "Welcome !! "+user.getUserName());
 		return "info";
 	}
